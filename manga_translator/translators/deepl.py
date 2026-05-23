4d6077903a5056e2e09a5e9711756dddf04ef4a6
Romeolbl <leblondromeo@gmail.com>
2026-05-23 19:30:07 +0200

    fix: replace problematic unicode chars in DeepL output

--- manga_translator/translators/deepl.py
+++ manga_translator/translators/deepl.py
@@ -42,6 +42,16 @@ class DeeplTranslator(CommonTranslator):
         'UKR': 'UK'
     }
 
+    REPLACEMENTS = {
+        '\u00c6': "'",
+        '\u00e0': "'",
+        '\u00f9': '-',
+        '\u2019': "'",
+        '\u2018': "'",
+        '\u201c': '"',
+        '\u201d': '"',
+    }
+
     def __init__(self):
         super().__init__()
         if not DEEPL_AUTH_KEY:
@@ -49,4 +59,13 @@ class DeeplTranslator(CommonTranslator):
         self.translator = deepl.Translator(DEEPL_AUTH_KEY)
 
     async def _translate(self, from_lang, to_lang, queries):
-        return self.translator.translate_text('\n'.join(queries), target_lang = to_lang).text.split('\n')
+        translated = self.translator.translate_text('\n'.join(queries), target_lang=to_lang).text
+        results = translated.split('\n')
+        fixed = []
+        for r in results:
+            for src, dst in self.REPLACEMENTS.items():
+                r = r.replace(src, dst)
+            # Garde uniquement les caractères ASCII + latin étendu courant
+            r = ''.join(c if ord(c) < 0x2000 else '' for c in r)
+            fixed.append(r)
+        return fixed
